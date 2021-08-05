8b51e766107425eb1fd699df683f57aa1e4e4182
driftboat <zhengshu1.1@163.com>
2021-08-05 16:24:46 +0800

    add checkAndDownloadSDK

--- zoom/bin/unzip_zoom_sdk.dart
+++ zoom/bin/unzip_zoom_sdk.dart
@@ -1,13 +1,18 @@
 import 'dart:core';
 import 'dart:io';
 import 'package:archive/archive.dart';
+import 'package:dio/dio.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  var location = Platform.script
+      .toString()
+      .replaceFirst("file:///", "")
+      .replaceFirst("file://", "")
+      .replaceFirst("/bin/unzip_zoom_sdk.dart", "");
+  var filename =
+      location + '/ios-sdk/MobileRTC${(args.length == 0) ? "" : "-dev"}.zip';
 
-  var location = Platform.script.toString().replaceFirst("file://", "").replaceFirst("/bin/unzip_zoom_sdk.dart", "");
-
-  var filename = location + '/ios-sdk/MobileRTC${(args.length == 0) ? "" : "-dev"}.zip';
-
+  await checkAndDownloadSDK(location);
   print('Decompressing ' + filename);
 
   final bytes = File(filename).readAsBytesSync();
@@ -28,4 +33,51 @@ void main(List<String> args) {
   }
 
   print('Complete');
+}
+
+Future<void> checkAndDownloadSDK(String location) async {
+  var iosSDKFile = location + '/ios-sdk/MobileRTC.zip';
+  bool exists = await File(iosSDKFile).exists();
+
+  if (!exists) {
+    await downloadFile(
+        Uri.parse(
+            'https://raw.githubusercontent.com/driftboat/flutter_zoom/main/zoom/ios-sdk/MobileRTC.zip'),
+        iosSDKFile);
+  }
+
+  var iosDevSDKFile = location + '/ios-sdk/MobileRTC-dev.zip';
+  exists = await File(iosDevSDKFile).exists();
+  if (!exists) {
+    await downloadFile(
+        Uri.parse(
+            'https://raw.githubusercontent.com/driftboat/flutter_zoom/main/zoom/ios-sdk/MobileRTC-dev.zip'),
+        iosDevSDKFile);
+  }
+
+  var androidCommonLibFile = location + '/android/libs/commonlib.aar';
+  exists = await File(androidCommonLibFile).exists();
+  if (!exists) {
+    await downloadFile(
+        Uri.parse(
+            'https://raw.githubusercontent.com/driftboat/flutter_zoom/main/zoom/android/libs/commonlib.aar'),
+        androidCommonLibFile);
+  }
+  var androidRTCLibFile = location + '/android/libs/mobilertc.aar';
+  exists = await File(androidRTCLibFile).exists();
+  if (!exists) {
+    await downloadFile(
+        Uri.parse(
+            'https://raw.githubusercontent.com/driftboat/flutter_zoom/main/zoom/android/libs/mobilertc.aar'),
+        androidRTCLibFile);
+  }
+}
+
+Future<void> downloadFile(Uri uri, String savePath) async {
+  print('Download $uri.toString() to $savePath');
+  var dio = Dio();
+  dio.options.connectTimeout = 1000000;
+  dio.options.receiveTimeout = 1000000;
+  dio.options.sendTimeout = 1000000;
+  await dio.downloadUri(uri, savePath);
 }
