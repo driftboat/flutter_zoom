a0324823f9bc3642adda5333be7cc22414e42101
Mauricio barros <mau.barros@outlook.com>
2021-08-11 15:37:12 -0300

    Implementation for zoom_platform_interface 1.0.2

--- zoom_platform_interface/CHANGELOG.md
+++ zoom_platform_interface/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.2
+* Add new options to use on ZoomMtg.init() (Web implementation)
+
 ## 1.0.1
 * add jwtAPIKey&jwtSignature to ZoomMeetingOptions
 
--- zoom_platform_interface/lib/zoom_options.dart
+++ zoom_platform_interface/lib/zoom_options.dart
@@ -1,8 +1,75 @@
 class ZoomOptions {
   String domain;
   String jwtToken;
+  String? language; //--for web
+  bool? showMeetingHeader; //--for web
+  bool? disableInvite; //--for web
+  bool? disableCallOut; //--for web
+  bool? disableRecord; //--for web
+  bool? disableJoinAudio; //--for web
+  bool? audioPanelAlwaysOpen; //--for web
+  bool? isSupportAV; //--for web
+  bool? isSupportChat; //--for web
+  bool? isSupportQA; //--for web
+  bool? isSupportCC; //--for web
+  bool? isSupportPolling; //--for web
+  bool? isSupportBreakout; //--for web
+  bool? screenShare; //--for web
+  String? rwcBackup; //--for web
+  bool? videoDrag; //--for web
+  String? sharingMode; //--for web
+  bool? videoHeader; //--for web
+  bool? isLockBottom; //--for web
+  bool? isSupportNonverbal; //--for web
+  bool? isShowJoiningErrorDialog; //--for web
+  bool? disablePreview; //--for web
+  bool? disableCORP; //--for web
+  String? inviteUrlFormat; //--for web
+  bool? disableVOIP; //--for web
+  bool? disableReport; //--for web
+  List<String>? meetingInfo; //--for web
 
-  ZoomOptions({required this.domain, required this.jwtToken});
+  ZoomOptions(
+      {required this.domain,
+      required this.jwtToken,
+      this.language = "en-US",
+      this.showMeetingHeader = true,
+      this.disableInvite = false,
+      this.disableCallOut = false,
+      this.disableRecord = false,
+      this.disableJoinAudio = false,
+      this.audioPanelAlwaysOpen = false,
+      this.isSupportAV = true,
+      this.isSupportChat = true,
+      this.isSupportQA = true,
+      this.isSupportCC = true,
+      this.isSupportPolling = true,
+      this.isSupportBreakout = true,
+      this.screenShare = true,
+      this.rwcBackup = '',
+      this.videoDrag = true,
+      this.sharingMode = 'both',
+      this.videoHeader = true,
+      this.isLockBottom = true,
+      this.isSupportNonverbal = true,
+      this.isShowJoiningErrorDialog = true,
+      this.disablePreview = false,
+      this.disableCORP = true,
+      this.inviteUrlFormat = '',
+      this.disableVOIP = false,
+      this.disableReport = false,
+      this.meetingInfo = const [
+        'topic',
+        'host',
+        'mn',
+        'pwd',
+        'telPwd',
+        'invite',
+        'participant',
+        'dc',
+        'enctype',
+        'report'
+      ]});
 }
 
 class ZoomMeetingOptions {
@@ -18,23 +85,23 @@ class ZoomMeetingOptions {
   String disableShare;
   String noDisconnectAudio;
   String noAudio;
-  String? jwtAPIKey;//--for web
-  String? jwtSignature;//--for web
+  String? jwtAPIKey; //--for web
+  String? jwtSignature; //--for web
 
-  ZoomMeetingOptions(
-      {required this.userId,
-      required this.meetingId,
-      required this.meetingPassword,
-      this.displayName,
-      this.zoomToken,
-      this.zoomAccessToken,
-      required this.disableDialIn,
-      required this.disableDrive,
-      required this.disableInvite,
-      required this.disableShare,
-      required this.noDisconnectAudio,
-      required this.noAudio,
-      this.jwtAPIKey,
-      this.jwtSignature,
-      });
+  ZoomMeetingOptions({
+    required this.userId,
+    required this.meetingId,
+    required this.meetingPassword,
+    this.displayName,
+    this.zoomToken,
+    this.zoomAccessToken,
+    required this.disableDialIn,
+    required this.disableDrive,
+    required this.disableInvite,
+    required this.disableShare,
+    required this.noDisconnectAudio,
+    required this.noAudio,
+    this.jwtAPIKey,
+    this.jwtSignature,
+  });
 }
